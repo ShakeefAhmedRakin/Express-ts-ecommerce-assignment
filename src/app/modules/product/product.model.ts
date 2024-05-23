194d8bbfc50720aa2348916e84bb036b66dc736c
ShakeefAhmedRakin <shakeef.rakin321@gmail.com>
2024-05-23 14:52:47 +0600

    Initialized project with eslinting and prettier formatting. Added Product type and schemas and model.

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import { Inventory, Product, Variant } from "./product.interface";
+
+const InventorySchema = new Schema<Inventory>({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+const VariantSchema = new Schema<Variant>({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+});
+
+const ProductSchema: Schema = new Schema<Product>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [VariantSchema], required: true },
+  inventory: { type: InventorySchema, required: true },
+});
+
+const Product = model<Product>("Product", ProductSchema);
